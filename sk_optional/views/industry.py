e0d71556edb8ed1e5dabe4b8916818713bfbf1fc
王慧哲 <huizhe.wang@bl.com>
2019-02-28 14:40:47 +0800

    语言识别

--- sk_optional/views/industry.py
+++ sk_optional/views/industry.py
@@ -13,73 +13,74 @@ from basicdata.models import StockPrice
 
 
 class IndustryViewSet(APIView):
-    """行业行情分析"""
-    analysis_data = {
-        '连板': {},
+    """市场分析"""
+    market_code = {
+        'fangliang': [],
+        'shangzhang': [],
+        'gainian': {}
     }
-    trading_day = []
-
 
     def get(self, request):
-        """行业行情分析"""
-        data = request.GET
-        if data and 'time' in data:
-            industry_name = cache.get(f'industry_coce_cache_{data["time"]}')
-        else:
-            industry_name = {}
-            url = f"{settings.QT_URL3}data/view/bdrank.php?&t=02/averatio&p=1&o=0&l=30&v=list_data"
-            url_info1 = self._open_url(url)
-            if url_info1:
-                code_info1 = url_info1.split("data:'")[1].replace("'};", '').replace('qt', 'hz')
-                # 行业祥情
-                url = f"{settings.QT_URL2}q={code_info1}&r=11192163"
-                url_info2 = self._open_url(url)
-                if url_info2:
-                    industry_list = re.findall('".*"', url_info2)
-                    for i in industry_list:
-                        industry_info = str(i).replace('"', '').split('~')
-                        industry_name[industry_info[0]] = {
-                            'name': industry_info[1],
-                            'no.1': industry_info[11],
-                            'new_code': []
-                        }
-                    # 行业各股
-                    for i in code_info1.split(','):
-                        url = f"{settings.QT_URL3}data/index.php?appn=rank&t={i.replace('bkhz', 'pt')}" \
-                              f"/chr&p=1&o=0&l=6&v=list_data"
-                        url_info3 = self._open_url(url)
-                        if url_info3:
-                            # 资金数据
-                            code_info = url_info3.split("data:'")[1].replace("'};", '')
-                            for code in code_info.split(','):
-                                read_code = cache.get(f'cache_code_info_{code[:2]}~{code[2:]}')
-                                if read_code:
-                                    if read_code['new'] == 1:
-                                        industry_name[i[4:]]['new_code'].append(read_code)
-            cache.set(f'industry_coce_cache_{datetime.date.today()}', industry_name, timeout=None)
-        return Response({"Industry": {"param": industry_name}})
+        """市场分析"""
+        self._fangliang()
+        self._shangzhang()
+        self._gainian()
 
-    def _continuous_rise(self):
+        return Response({'market_code': self.market_code})
+
+    def _gainian(self):
+        """概念"""
+        url = f"{settings.QT_URL3}data/view/bdrank.php?&t=02/averatio&p=1&o=0&l=30&v=list_data"
+        url_info1 = self._open_url(url)
+        if url_info1:
+            code_info1 = url_info1.split("data:'")[1].replace("'};", '').replace('qt', 'hz')
+            # 行业祥情
+            url = f"{settings.QT_URL2}q={code_info1}&r=11192163"
+            url_info2 = self._open_url(url)
+            if url_info2:
+                industry_list = re.findall('".*"', url_info2)
+                for i in industry_list:
+                    industry_info = str(i).replace('"', '').split('~')
+                    self.market_code['gainian'][industry_info[0]] = {
+                        'name': industry_info[1],
+                        'no.1': industry_info[11],
+                        'new_code': []
+                    }
+                # 行业各股
+                for i in code_info1.split(','):
+                    url = f"{settings.QT_URL3}data/index.php?appn=rank&t={i.replace('bkhz', 'pt')}" \
+                        f"/chr&p=1&o=0&l=6&v=list_data"
+                    url_info3 = self._open_url(url)
+
+                    find_code = re.findall("'.*'", url_info3)
+                    if find_code:
+                        gainian_code = find_code[0][:9][3:]
+                        if gainian_code in self.market_code['gainian']:
+                            self.market_code['gainian'][gainian_code]['new_code'].append(
+                                [i[2:] for i in str(find_code[0]).replace("'", '').split('data:')[1].split(',')]
+                            )
+        return None
+
+    def _shangzhang(self):
         """连续上涨"""
-        td = trading_day(6)
         url = f'{settings.QT_URL3}data/view/dataPro.php?t=2&p=3'
         url_info = self._open_url(url)
         find_code = re.findall("'.*'", url_info)
         if find_code:
             code_list = str(find_code[0]).replace("'", '').split('^')
-            for i in code_list:
-                code_query = Base(StockPrice, **{'code': str(i).split('~')[0][2:], 'trading_day__in': td}).findfilter()
-                print(str(i).split('~')[0][2:])
-                print(code_query)
-                print([i.average for i in code_query])
-                print(i)
-                return None
+            for code in code_list:
+                self.market_code['shangzhang'].append((code.split('~')[0][2:], code.split('~')[1]))
+        return None
 
-    def _accelerated(self):
+    def _fangliang(self):
         """突放量"""
         url = f'{settings.QT_URL3}data/view/dataPro.php?t=7&p=1'
         url_info = self._open_url(url)
-
+        find_code = re.findall("'.*'", url_info)
+        if find_code:
+            code_list = str(find_code[0]).replace("'", '').split('^')
+            for code in code_list:
+                self.market_code['fangliang'].append(code.split('~')[0][2:])
         return None
 
     def _open_url(self, url):
@@ -90,4 +91,3 @@ class IndustryViewSet(APIView):
             return url_info
         else:
             return None
-
